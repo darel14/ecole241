2e353a6241e625d51de47736f77a0ae527b880fb
Ebaneth <ebaneth1402@gmail.com>
2019-02-18 16:04:25 +0100

    new file

---
+++ sprint/s6.js
@@ -0,0 +1,9 @@
+ var number = prompt ("entrez votre ages !")
+       
+            if (number>21) {
+                console.log ("vous etes majeur");
+            }
+            else
+            {
+                console.log ("vous etes mineur");
+            }

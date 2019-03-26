5fc1d8095fae5d47d7c56477cd6fa48a9a1cfe75
Ebaneth <ebaneth1402@gmail.com>
2019-03-26 15:33:53 +0100

    new file

---
+++ sprint/php/act2.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+
+<head>
+<title>test de php</title>
+ <meta charset="utf-8">
+   </head>
+
+<body>
+
+<?php
+    
+    $activiter = "mon activite";   
+    $occupation  = "Dessin" ; 
+    
+    $pseudo2= "mon pseudo";
+    $pseudo=  "kog142" ; 
+    
+    $mon= "mon activité";
+    $activite = "etudiant en securite informatique";
+    
+    echo  " $activiter, $occupation<br> , $pseudo2, $pseudo<br>, $mon ,$activite "; ?>
+ 
+  </body>

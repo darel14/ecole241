b5c5e61f8424b2cd897519c3d4840c5875714d2f
Ebaneth <ebaneth1402@gmail.com>
2019-03-27 10:13:33 +0100

    new file

---
+++ sprint/php/act3.php
@@ -0,0 +1,13 @@
+<?php 
+$etudiant = array("Ebaneth", "Rick", "Kevan", "Junior", "Darel" , "Pauline" , "Capitaine" , "kuroro" , "Kakarot" , "Katchan" );
+echo $etudiant[0].'<br>';
+echo $etudiant[1].'<br>';
+echo $etudiant[2].'<br>';
+echo $etudiant[3].'<br>';
+echo $etudiant[4].'<br>';
+echo $etudiant[5].'<br>';
+echo $etudiant[6].'<br>';
+echo $etudiant[7].'<br>';
+echo $etudiant[8].'<br>';
+echo $etudiant[9].'<br>';
+?>

e34166ac59facb66ef744a7b08fbc4540e1a55f9
Ebaneth <ebaneth1402@gmail.com>
2019-04-01 11:51:12 +0200

    new file

--- sprint/php/act4.php
+++ sprint/php/act4.php
@@ -7,25 +7,25 @@
     </head>
 <body>
 <?php 
-$tableau= array( 'nom' => 'françois', 'age' => '35', 'nationnalité' => 'ivoirienne', 'profil' => 'https://bernyfrancois.github.io/ecole241/profil/');
+$tableau= array( 'nom' => 'françois', 'age' => 35, 'nationnalité' => 'ivoirienne', 'profil' => 'https://bernyfrancois.github.io/ecole241/profil/');
+    
+        => array( 'nom' => 'Jboumis', 'age' => 45, 'nationnalité' => 'Gabonaise', 'profil' => 'http://jboumys.github.io/ecole241/profil/');
 
-$tableau= array( 'nom' => 'Jboumis', 'age' => '45', 'nationnalité' => 'Gabonaise', 'profil' => 'http://jboumys.github.io/ecole241/profil/');
+        => array( 'nom' => 'Billy Mark', 'age' => 38, 'nationnalité' => 'congolaise', 'profil' => 'https://light-int.github.io/ecole241/profil/index.html' );
 
-$tableau= array( 'nom' => 'Billy Mark', 'age' => '38', 'nationnalité' => 'congolaise', 'profil' => 'https://light-int.github.io/ecole241/profil/index.html' );
+        => array( 'nom' => 'Albert sisso', 'age' => 38, 'nationnalité' => 'Gabonaise', 'profil' => 'https://albert241.github.io/ecole241/profil/');
 
-$tableau= array( 'nom' => 'Albert sisso', 'age' => '38', 'nationnalité' => 'Gabonaise', 'profil' => 'https://albert241.github.io/ecole241/profil/');
+        => array( 'nom' => 'Pauline', 'age' => 38, 'nationnalité' => 'Sud africaine', 'profil' => 'https://nzebitome.github.io/ecole241/profil/');
 
-$tableau= array( 'nom' => 'Pauline', 'age' => '38', 'nationnalité' => 'Sud africaine', 'profil' => 'https://nzebitome.github.io/ecole241/profil/');
+        => array( 'nom' => 'Jmaurio', 'age' => 28, 'nationnalité' => 'Gabonaise', 'profil' => 'https://joemaurio.github.io/ecole241/profil/');
 
-$tableau= array( 'nom' => 'Jmaurio', 'age' => '28', 'nationnalité' => 'Gabonaise', 'profil' => 'https://joemaurio.github.io/ecole241/profil/');
+        => array( 'nom' => 'Amilton Darryl', 'age' => 55, 'nationnalité' => 'Gabonaise', 'profil' => 'https://hamiltondarryl.github.io/ecole241/profil/' );
 
-$tableau= array( 'nom' => 'Amilton Darryl', 'age' => '55', 'nationnalité' => 'Gabonaise', 'profil' => 'https://hamiltondarryl.github.io/ecole241/profil/' );
+        => array( 'nom' => 'Grace', 'age' => 48, 'nationnalité' => 'ivoirienne', 'profil' => 'https://lacastafiore.github.io/ecole241/profil/');
 
-$tableau= array( 'nom' => 'Grace', 'age' => '48', 'nationnalité' => 'ivoirienne', 'profil' => 'https://lacastafiore.github.io/ecole241/profil/');
+        => array( 'nom' => 'Lauryon', 'age' => 18, 'nationnalité' => 'ivoirienne', 'profil' => 'https://lacastafiore.github.io/ecole241/okacode/images/Laurion.jpg' );
 
-$tableau= array( 'nom' => 'Lauryon', 'age' => '18', 'nationnalité' => 'ivoirienne', 'profil' => 'https://lacastafiore.github.io/ecole241/okacode/images/Laurion.jpg' );
-
-$tableau= array( 'nom' => 'Didier Capitaine', 'age' => '60', 'nationnalité' => 'senegalaise', 'profil' => 'https://lacastafiore.github.io/ecole241/okacode/images/Didier.jpg' );
+        => array( 'nom' => 'Didier Capitaine', 'age' => 60, 'nationnalité' => 'senegalaise', 'profil' => 'https://lacastafiore.github.io/ecole241/okacode/images/Didier.jpg' );
 ?>
     </body>
 </html>
